6eb1b52f8d53e7f5faaf8567bf2e2b3970b0e5e7
Andres Galvan <afgalvan@unicesar.edu.co>
2021-02-10 17:36:24 -0500

    JavaFX CLI compilation script

---
+++ .scripts/javafx.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+export JAVAFX_PATH="$HOME/.javafx"
+
+javafx() {
+    local command="$1"
+    local target="$2"
+    
+    if [ -z "$3" ]; then
+        local javafx_version="11.0.2"
+    else
+        local javafx_version="$3"
+    fi
+    
+    $command --module-path ~/.javafx/javafx-sdk-$javafx_version/lib --add-modules javafx.controls,javafx.fxml "$target"
+}
+
+compile_javafx() {
+    local class_name="$1"
+    local javafx_version="$2"
+    
+    echo -e "Compiling...\n"
+    javafx "javac" "$class_name" "$javafx_version"
+}
+
+run_javafx() {
+    local class_name=$(echo "$1" | cut -d '.' -f 1)
+    local javafx_version="$2"
+    
+    echo -e "Running...\n"
+    javafx "java" "$class_name" "$javafx_version"
+}
+
+main() {
+    if [ "$1" == "-v" ]; then
+        ls "$JAVAFX_PATH"
+        exit 0
+    fi
+    
+    compile_javafx "$@" && run_javafx "$@" || echo -e "\e[31mError on javafx compilation"; exit 1
+}
+
+main "$@"
+
