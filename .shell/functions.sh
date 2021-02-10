0cb7fdcc914561e02a1bfd31aad316ee9cba2a4d
Andres Galvan <afgalvan@unicesar.edu.co>
2021-02-10 17:37:41 -0500

    Extraction function

--- .shell/functions.sh
+++ .shell/functions.sh
@@ -30,13 +30,7 @@ g__run() {
     local file_name="$2"
     local out_name=$(echo $file_name | cut -d '.' -f 1)
     
-    {
-        $compiler "$file_name" -o "$out_name.out"
-        } && {
-        ./"$out_name.out"
-        } || {
-        echo ""
-    }
+    $compiler "$file_name" -o "$out_name.out" && ./"$out_name.out"
 }
 
 crun() {
@@ -60,6 +54,29 @@ conanrun() {
     cbuild "$1"
     bin/"$1"
     cd -
+}
+
+extract () {
+    if [ -f $1 ]; then
+        case $1 in
+            *.tar.bz2)  tar -jxvf $1                        ;;
+            *.tar.gz)   tar -zxvf $1                        ;;
+            *.bz2)      bunzip2 $1                          ;;
+            *.gz)       gunzip $1                           ;;
+            *.tar)      tar -xvf $1                         ;;
+            *.tbz2)     tar -jxvf $1                        ;;
+            *.tgz)      tar -zxvf $1                        ;;
+            *.zip)      unzip $1                            ;;
+            *.ZIP)      unzip $1                            ;;
+            *.pax)      cat $1 | pax -r                     ;;
+            *.pax.Z)    uncompress $1 --stdout | pax -r     ;;
+            *.rar)      unrar x $1                          ;;
+            *.Z)        uncompress $1                       ;;
+            *)          echo "\"$1\" cannot be extracted/mounted via extract()" ;;
+        esac
+    else
+        echo "\"$1\" is not a valid file"
+    fi
 }
 
 function gi() { curl -sLw n https://www.toptal.com/developers/gitignore/api/$* ;}
